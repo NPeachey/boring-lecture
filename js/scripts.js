249819f96a0adc92b605983ea24364fd51fad8f2
Ahmed Khokar and neil peachey <ahmed@>
2018-08-07 14:23:53 -0700

    added a yellow background and black border to paragraphs with a click

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("p#highlight1").click(function() {
+    $("p#highlight1").addClass("yellow-background")
+  });
+  $("p#highlight2").click(function() {
+    $("p#highlight2").addClass("yellow-background")
+  });
+});
